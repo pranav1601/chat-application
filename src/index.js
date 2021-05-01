0972ff58612ae3f4c4ad3c0de99de6c1dfd2be37
pranav1601 <pranav.agarw@gmail.com>
2021-05-01 16:33:42 +0530

    added socket.io base for the application

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express=require('express')
 const path=require('path')
+const http=require('http')
+const socketio=require('socket.io')
+
+const app=express()
+const server=http.createServer(app)
+const io=socketio(server)
 
 const port=process.env.PORT || 3000
 const publicDirectoryPath=path.join(__dirname,'../public')
 
-const app=express()
-
 app.use(express.static(publicDirectoryPath))
 
+io.on('connection',()=>{
+    console.log('new connection')
+})
 
-
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`server is up on ${port}`)
 })
