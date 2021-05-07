5899ad33d5f43b73cddf97d4dd8fd23509f867c1
pranav1601 <pranav.agarw@gmail.com>
2021-05-07 06:37:46 +0530

    disabled send location and send message when bacground emit occurring

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,9 @@
 const socket=io()
 
 const chatForm=document.querySelector('#message-form')
+const chatFormInput=chatForm.querySelector('input')
+const chatFormButton=chatForm.querySelector('button')
+
 const sendLocation=document.querySelector('#send-location')
 
 socket.on('message',(message)=>{
@@ -9,8 +12,14 @@ socket.on('message',(message)=>{
 
 chatForm.addEventListener('submit',(e)=>{
     e.preventDefault()
+
+    chatFormButton.setAttribute('disabled','disabled')
+
     const message=e.target.elements.message
     socket.emit('sendMessage',message.value,(error)=>{
+        chatFormButton.removeAttribute('disabled')
+        chatFormInput.value=''
+        chatFormInput.focus()
         if(error){
             return console.log(error)
         }
@@ -19,12 +28,16 @@ chatForm.addEventListener('submit',(e)=>{
 })
 
 sendLocation.addEventListener('click',()=>{
+    
+    
     if(!navigator.geolocation){
         return alert('geolocation not supported by your browser')
     }
+    sendLocation.setAttribute('disabled','disabled')
     navigator.geolocation.getCurrentPosition((position)=>{
         const {latitude,longitude}=position.coords
         socket.emit("sendLocation",{latitude,longitude},(error)=>{
+            sendLocation.removeAttribute('disabled')
             if(error){
                 return console.log(error)
             }
