c17201670208f8a365410f07be6472d16a5076b7
pranav1601 <pranav.agarw@gmail.com>
2021-05-08 20:40:55 +0530

    location to be rendered in browser page as a link which opens in new tab

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,11 +8,20 @@ const sendLocation=document.querySelector('#send-location')
 const messages=document.querySelector("#messages")
 
 const messageTemplate=document.querySelector('#message-template').innerHTML
+const locationTemplate=document.querySelector('#location-template').innerHTML
 
 socket.on('message',(message)=>{
     console.log(message)
     const html=Mustache.render(messageTemplate,{
         message
+    })
+    messages.insertAdjacentHTML('beforeend',html)
+})
+
+socket.on('locationMessage',(location)=>{
+    console.log(location)
+    const html=Mustache.render(locationTemplate,{
+        location
     })
     messages.insertAdjacentHTML('beforeend',html)
 })
