93060a6e461c7e9104d86d99ab660d6d4fe0c88e
pranav1601 <pranav.agarw@gmail.com>
2021-05-09 02:11:51 +0530

    integrated util funcitons to code for creating room user adding and removal funcitonality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const http=require('http')
 const socketio=require('socket.io')
 const Filter=require('bad-words')
 const {generateMessage,generateLocationMessage}=require('./utils/messages')
+const {getUser,getUsersInRoom,addUser,removeUser}=require('./utils/users')
 
 const app=express()
 const server=http.createServer(app)
@@ -18,11 +19,18 @@ io.on('connection',(socket)=>{
     console.log('new connection')
 
 
-    socket.on('join',({username,room})=>{
-        socket.join(room)
+    socket.on('join',(options,callback)=>{
+        const {error,user}=addUser({id:socket.id,...options})
+        if(error){
+            return callback(error)
+        }
+
+        socket.join(user.room)
 
         socket.emit('message',generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message',generateMessage(`${username} has joined!`))
+        socket.broadcast.to(user.room).emit('message',generateMessage(`${user.username} has joined!`))
+
+        callback()
     })
 
     socket.on('sendMessage',(message, callback)=>{
@@ -40,7 +48,11 @@ io.on('connection',(socket)=>{
     })
 
     socket.on('disconnect',()=>{
-        io.emit('message',generateMessage('user has left'))
+        const user=removeUser(socket.id)
+
+        if(user){
+            io.to(user.room).emit('message',generateMessage(`${user.username} has left!`))
+        }
     })
 })
 
